7a67339e78ea41808a039dad7ed1f1d7c5faddac
Guilherme Fontes <guilhermefamorim2020@gmail.com>
2020-05-05 23:12:53 -0400

    refactor: adicionado ferramentas para commits

--- src/server.js
+++ src/server.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
     return res.json({ users })
 });
 
-app.listen(80, () => {
-    console.log('App listening on port 80!');
+app.listen(3000, () => {
+    console.log('App listening on port 3000!');
 });
