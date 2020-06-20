ad60ce29657c1582c374aec9f67975e972ef3be7
Guilherme Fontes <guilhermefamorim2020@gmail.com>
2020-06-20 02:16:49 -0400

    feat(package): adicionado o cookie-parser
    Implementado a dependencia do cookie-parser juntamento com seu uso no servidor

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import exphbs from 'express-handlebars'
 import path from 'path'
+import cookie_parser from 'cookie-parser'
+
 require('./database')
 
 const app = express()
@@ -12,6 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cookie_parser())
 
 require('./controllers/index')(app)
 
