448223c5547de3a7a536971b9f034ec1d78f03dd
Guilherme Fontes <guilhermefamorim2020@gmail.com>
2020-08-23 03:11:51 -0400

    feat(controllers): criado a tabela de administração
    Foi criado a tabala onde os moderadores e administradores poderam gerenciar seus alunos e seus
    certificado.
    Faltando apenas aprovar/reprovar/desfazer o aluno e editar o tipo,valor do certificado
    para a pagina ficar 100%

---
+++ src/controllers/admin.js
@@ -0,0 +1,196 @@
+import express from 'express'
+import User from '../models/user' 
+import { Op } from 'sequelize'
+import Certified from '../models/certified'
+import TypeCertified from '../models/type_certified'
+
+const router = express.Router()
+
+
+router.use((req, res, next) => {
+    if(!req.session.user){
+        res.redirect('/login')
+    }else if(req.session.user.tipo_usuario <= 2){
+        next()
+    }else{
+        res.redirect('/dashboard')
+    }    
+})
+
+router.get('/procurar', async (req, res) => { 
+    var aluno = null
+    var certificadoAluno = null
+    var total = 0;
+    if(req.session.mostrarTabela){
+        await User.findByPk(req.session.mostrarTabela).then((response) => {
+            if(response){
+                aluno = {
+                    id: response.getDataValue('id'),
+                    email: response.getDataValue('email_user'),
+                    nome: response.getDataValue('name_user'),
+                    curso: response.getDataValue('course_user'),
+                    semestre: response.getDataValue('half_user'),
+                    estado: response.getDataValue('id_state_foreign')
+                } 
+            }            
+        })
+
+        await Certified.findAll({
+            include: [
+                {
+                    model: TypeCertified,
+                    required: true,
+                },
+            ],
+            where: {
+                id_user_foreign: aluno.id
+            }
+        }).then(response => {
+            certificadoAluno = response.map(c => {
+                return Object.assign({},{
+                    id: c.id,
+                    nome: c.name_certified,
+                    valor: c.value_certified,
+                    foto: c.picture_certified,
+                    numero_tipo: c.id_type_certified_foreign,
+                    tipo: c.type_certified.get('name_type_certified'),
+                    estado: c.id_state_foreign
+                })
+            })
+        })
+
+        certificadoAluno.forEach(c => {
+            if(c.estado == 1)
+                total += c.valor 
+        })
+
+    }
+
+    req.session.mostrarTabela = undefined
+
+    res.render('procurar_aluno', {
+        js: 'controllers_js/procurar_aluno.js',
+        style: 'controllers_css/dashboard.css',
+        title: 'UniCertified | Procurar Aluno',
+        user: req.session.user,
+        aluno: aluno,
+        total: total,
+        certificados: certificadoAluno,
+        breadcrumb: `Procurar Aluno`,
+    }) 
+    
+})
+
+router.get('/procurar/alunos', async (req, res) => {
+    /* var contador = 0
+    var usuarios = [] */
+    const { count ,rows } =  await User.findAndCountAll({
+        where: {
+            name_user: {
+                [Op.like]: `${req.query['q']}%`
+            }
+        }
+    })
+
+    var usuarios =  {}
+    var key = 'user'
+    usuarios[key] = []
+
+    rows.forEach(element => {
+        usuarios[key].push({
+            id: element.getDataValue('id'),
+            name: element.getDataValue('name_user'),
+            email: element.getDataValue('email_user')
+        })
+    });
+
+    res.send(JSON.stringify(usuarios))
+    
+})
+
+router.post('/procurar/criartabela', async (req, res) => {
+    req.session.mostrarTabela = req.body.id
+    req.session.alunoTabela = req.body.id
+    res.send({ success: 'Tabela sendo criada'})
+})
+
+router.post('/procurar/alterarestado', async (req, res) => {
+    var { id , comentario, acao, id_aluno } = req.body
+    var count_error = 0
+    var acao = acao != 1 ? 3:1
+
+    if(!(comentario != ' ' && comentario.length <= 350))
+        count_error += 1        
+    if(!(id != ' '))
+        count_error += 1    
+    if(req.session.alunoTabela != id_aluno)
+        count_error += 1 
+
+    console.log(req.session.alunoTabela);
+
+    if(count_error != 0)
+        res.status(200).json({ error: 'Ocorreu um erro nos dados enviados'})
+    else{
+        await Certified.update(
+            {
+                comments_certified: comentario,
+                id_state_foreign: acao,
+                updated_at: new Date()
+            },
+            {
+                where: {
+                    id
+                }
+            }
+        ).then(response => {
+            if(response){
+                if(acao == 1)
+                    res.send({ success: 'Certificado aprovado com sucesso'})
+                else
+                    res.send({ success: 'Certificado reprovado com sucesso'})
+            }else
+                res.send({ error: 'Ocorreu um erro interno'})
+                
+        })
+    }
+    
+
+})
+
+router.post('/procurar/habilitaredicao', async (req,res) => {
+    const { id, id_aluno } = req.body
+    var count_error = 0
+
+    if(!(id != ' '))
+        count_error += 1
+    if(!(id_aluno != ' '))
+        count_error += 1
+    if(req.session.alunoTabela != id_aluno)
+        count_error += 1
+    
+    if(count_error != 0)
+        res.send({ error: 'Ocorreu um erro interno'})
+    else{
+        Certified.update(
+            {
+                id_state_foreign: 2,
+                updated_at: new Date()
+            },
+            {
+                where: {
+                    id,
+                    id_user_foreign: id_aluno
+                }
+            }
+        ).then(response => {
+            if(response != 0)
+                res.send({ success: 'Edição habilitada com sucesso'})
+            else
+                res.send({ error: 'Ocorreu um erro interno'})
+        })
+    }
+    
+
+})
+
+module.exports = app => app.use('/admin', router)
---
+++ src/public/js/controllers_js/procurar_aluno.js
@@ -0,0 +1,183 @@
+$( document ).ready(function() {
+    $('.sidebar-menu-toggler').on('click', function(){
+        var target = $(this).data('target')
+        $(target).sidebar({
+            dimPage: true,
+            transition: 'overlay',
+            mobileTransition: 'overlay'
+        }).sidebar('toggle')
+    })   
+
+    $('.comentario')
+        .popup({
+            inline: true
+        })
+    ;
+
+    $('.ui.form.alterarEstado')
+        .form({
+            inline: true,
+            on: 'blur',
+            fields: {
+                nome: {
+                    identifier: 'comentario',
+                    rules: [
+                        {
+                            type: 'empty',
+                            prompt: 'Informe um nome'
+                        },
+                        {
+                            type: 'maxLength[350]',
+                            prompt: 'O nome deve conter no máximo 40 caracteres.'
+                        }
+                    ]
+                },
+            }
+        })
+
+    $('.ui.search').search({
+            /* source : content, */
+            minCharacters: 3,
+            cache: true,
+            apiSettings: {
+                url: '/admin/procurar/alunos?q={query}',                       
+            },
+            fields: {
+                results : 'user',
+                description: 'email',
+                title: 'name',                
+            },
+            onSelect: function(result, response){
+                $.post('/admin/procurar/criartabela', {id: result.id}, (postResponse) => {
+                    location.reload()
+                })
+            }
+        },
+    )
+    ;
+
+    $('.mostrarImagemCertificado').on('click', function(){
+        var urlFoto = $(this).attr('data-foto')
+        if(urlFoto == 'http://localhost:3000/tmp/uploads/'){
+            swal({
+                closeOnEsc: false,
+                closeOnClickOutside: false,
+                title: 'Ocorreu algum erro ao tentar exibir a imagem',
+                icon: "error",
+            })
+        }else{
+            $('.modificarImagem').attr('src', urlFoto)
+            $('.ui.basic.modal.mostrarImagem').modal('show')
+        }    
+    })
+
+    $('button.habilitarEdicao').on('click', function(){
+        var id = $(this).data('id')
+        var id_aluno = $('.header.dataid').data('id')
+        $('.ui.mini.modal.habilitarEdicao')
+            .modal({
+                onDeny:function(){
+                    return true
+                },
+                onApprove:function(){
+                    $.post('/admin/procurar/habilitaredicao',
+                    {id, id_aluno},
+                    response => {
+                        if(response.success){
+                            swal({
+                                closeOnEsc: false,
+                                closeOnClickOutside: false,
+                                title: response.success,
+                                icon: "success",
+                            }).then(() => {
+                                $.post('/admin/procurar/criartabela', 
+                                    {id: id_aluno},
+                                    (response) => {
+                                        location.reload()
+                                    })
+                            })
+                        }else{
+                            swal({
+                                closeOnEsc: false,
+                                closeOnClickOutside: false,
+                                title: response.error,
+                                icon: "error",
+                            })
+                        }
+                    }    
+                    )
+                    return true
+                }
+            }).modal('show')
+    })
+
+    $('button.alterarEstado').on('click', function(){
+        var id = $(this).data("id")
+        var comentario = $('textarea[name="comentario"]')
+        var id_aluno = $('.header.dataid').data('id')
+        $('.ui.mini.modal.alterarEstado')
+            .modal({
+                onDeny    : function(){
+                    if($('.ui.form.alterarEstado').form('is valid')){
+                        $.post('/admin/procurar/alterarestado',{id,comentario: comentario.val(), acao: 0, id_aluno}, (response) => {
+                            if(response.success){
+                                swal({
+                                    closeOnEsc: false,
+                                    closeOnClickOutside: false,
+                                    title: response.success,
+                                    icon: "success",
+                                }).then(() =>{
+                                    $.post('/admin/procurar/criartabela', 
+                                    {id: id_aluno},
+                                    (response) => {
+                                        location.reload()
+                                    })
+                                })
+                            }else{
+                                swal({
+                                    closeOnEsc: false,
+                                    closeOnClickOutside: false,
+                                    title: response.error,
+                                    icon: "error",
+                                })
+                            }
+                        })
+                    }else
+                        return false
+
+                    comentario.val(' ')
+                    return true
+                },
+                onApprove : function() {
+                    if($('.ui.form.alterarEstado').form('is valid')){
+                        $.post('/admin/procurar/alterarestado',{id,comentario: comentario.val(), acao: 1, id_aluno}, (response) => {
+                            if(response.success){
+                                swal({
+                                    closeOnEsc: false,
+                                    closeOnClickOutside: false,
+                                    title: response.success,
+                                    icon: "success",
+                                }).then(() =>{
+                                    $.post('/admin/procurar/criartabela', 
+                                    {id: $('.header.dataid').data('id')},
+                                    (response) => {
+                                        location.reload()
+                                    })
+                                })
+                            }else{
+                                swal({
+                                    closeOnEsc: false,
+                                    closeOnClickOutside: false,
+                                    title: response.error,
+                                    icon: "error",
+                                })
+                            }
+                        })
+                    }else  
+                        return false
+                    comentario.val(' ')
+                }
+            }
+            ).modal('show');
+    })
+});
